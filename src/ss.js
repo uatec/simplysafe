d95a397a2c4623109fbeaca33af82878d7153545
Robert Stiff <uatecuk@gmail.com>
2016-05-11 08:52:43 +0100

    discover tests across the whole project

--- src/ss.js
+++ src/ss.js
@@ -4,13 +4,15 @@ var glob = require('glob');
 var reporter = require('./simplereporter.js');
 var path = require('path');
         
+var workingDir = process.argv[1];
+
 // find files in current or target dir
-glob('**/*.test.js', function (err, items) {
+glob(path.join(workingDir, '**/*.test.js'), function (err, items) {
     var testPlan = {};
     // load all these modules
     // build test plan
     for (var i = 0; i < items.length; i++) {
-        var testSuite = require(path.join(process.cwd(), items[i]));
+        var testSuite = require(path.join(workingDir, items[i]));
         for (var test in testSuite) {
             testPlan[items[i] + ' > ' + test] = testSuite[test];
         }
